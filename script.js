dc1a8e3a466e9aab6b94abb03a419b5bd7897607
Leonardo Henrique <code.leoh@gmail.com>
2024-08-24 16:10:34 -0300

    Incluindo os parametros da url para efetuar com exito a requisição

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('.busca').addEventListener('submit', (event) => {
     if(input !== '') {
         showWarning('Carregando...')
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${econdeURI(input)}&appid=b95fa5771bb30a97f7e77f16336dab71`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${econdeURI(input)}&appid=b95fa5771bb30a97f7e77f16336dab71&units=metric&lang=pt_br`;
     }
 });
 
