4a457b06bcce806757f30ffd90e47c5cb1539aa8
Leonardo Henrique <code.leoh@gmail.com>
2024-08-24 15:35:02 -0300

    Criado o input de pesquisa, a verificação de se caso o valor da pesquisa não for vasia para mostrar a msg e a variável da url da api

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector('.busca').addEventListener('submit', (event) => {
     event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if(input !== '') {
+        showWarning('Carregando...')
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${econdeURI(input)}&appid=b95fa5771bb30a97f7e77f16336dab71`;
+    }
 });
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
